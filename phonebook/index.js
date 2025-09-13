e58653d68d71a62420ece7c4a267536b7f026019
Marwan Tawfik <marwantawfik2048@gmail.com>
2025-09-13 19:21:02 +0300

    adding frontend production build for deployment

--- phonebook/index.js
+++ phonebook/index.js
@@ -3,8 +3,10 @@ const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 
+
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('body', (request, response) => {
     return JSON.stringify(request.body)
